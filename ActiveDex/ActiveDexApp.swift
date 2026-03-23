e972aaca9a61282696f48b0ffac317d6e5e27b47
Revanfer14 <revancrozz@gmail.com>
2026-03-23 20:14:46 +0700

    Initial Commit

---
+++ ActiveDex/ActiveDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  ActiveDexApp.swift
+//  ActiveDex
+//
+//  Created by Revan Ferdinand on 23/03/26.
+//
+
+import SwiftUI
+
+@main
+struct ActiveDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
